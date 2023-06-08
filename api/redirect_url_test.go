68764e042cdca8fd28f0cd6daaa8d06d5127fa88
Trevin Wisaksana <twisaksana@gmail.com>
2023-06-08 15:58:34 +0700

    Added redirect url unit test

--- api/redirect_url_test.go
+++ api/redirect_url_test.go
@@ -1,7 +1,58 @@
 package api
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	mockdb "github.com/trevinwisaksana/trevin-urlshortener/db/mock"
+)
 
 func TestRedirectURL(t *testing.T) {
+	url := dummyURL(t)
 
+	testCases := []struct {
+		name          string
+		id            string
+		buildStub     func(store *mockdb.MockStore)
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			id:   url.ID,
+			buildStub: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetLongURL(gomock.Any(), gomock.Eq(url.ID)).
+					Times(1)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusFound, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStub(store)
+
+			server := NewTestServer(t, store)
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/%v", tc.id)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
 }
--- db/sqlc/url_test.go
+++ db/sqlc/url_test.go
@@ -8,7 +8,7 @@ import (
 	tools "github.com/trevinwisaksana/trevin-urlshortener/tools"
 )
 
-func TestShortenURL(t *testing.T) {
+func generateShortURL(t *testing.T) (Url, error) {
 	randomID := tools.RandomString(5)
 	longUrl := "https://www.notion.so/stockbit/Backend-Engineering-Challenge-Link-Shortener-82bf71375701427c9cdd54a10a775ba6?pvs=4"
 	baseUrl := "http://localhost:8080/"
@@ -27,4 +27,22 @@ func TestShortenURL(t *testing.T) {
 	require.Equal(t, arg.ID, result.ID)
 	require.Equal(t, arg.LongUrl, result.LongUrl)
 	require.Equal(t, arg.ShortUrl, result.ShortUrl)
+
+	return result, err
+}
+
+func TestShortenURL(t *testing.T) {
+	generateShortURL(t)
+}
+
+func TestGetLongURL(t *testing.T) {
+	response, err := generateShortURL(t)
+	require.NoError(t, err)
+
+	result, err := testQueries.GetLongURL(context.Background(), response.ID)
+
+	require.NoError(t, err)
+	require.NotNil(t, result)
+
+	require.Equal(t, response.LongUrl, result)
 }
